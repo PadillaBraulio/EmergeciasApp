e20e9d41ec167f09e09f44e92e37519bef4050d0
Braulio <brauliopadilla@gmail.com>
2016-05-02 13:07:57 -0600

    Creación de TabLayout con ViewPager, para los menus

--- SOSapp/app/src/main/java/com/example/root/sosapp/MainActivity.java
+++ SOSapp/app/src/main/java/com/example/root/sosapp/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.root.sosapp;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager pager = null;
+    private TabAdapter pageradapter = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new TabAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+    public static class TabAdapter extends FragmentPagerAdapter
+    {
+        private final static int PAG_COUNT = 3;
+        private String [] tabTitles = {"Ubicación","Manuales","Donativos"};
+        private Context context;
+
+        public TabAdapter(FragmentManager fm, Context context)  {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position)
+            {
+
+                default: return DefaultFragment.newInstance(1 + position);
+
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // Generate title based on item position
+            return tabTitles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return PAG_COUNT;
+        }
     }
 }
